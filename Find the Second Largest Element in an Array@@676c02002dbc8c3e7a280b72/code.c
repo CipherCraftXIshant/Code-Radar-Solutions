b415aa800474118f024753bcb4008707eb7eafea
Ishant Sharma <ishantsharma9499@gmail.com>
2025-03-29 21:34:08 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,35 +1,37 @@
 #include <stdio.h>
 
-void findSecondLargest(int arr[], int size) {
-    int largest = arr[0], secondLargest = arr[0];
+void bubbleSort(int arr[], int n) {
+    int t;
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                t = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = t;
+            }
+        }
+    }
+}
 
-    for (int i = 1; i < size; i++) {
-        if (arr[i] > largest) {
-            secondLargest = largest;
-            largest = arr[i];
-        } else if (arr[i] > secondLargest && arr[i] < largest) {
+int main(){
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    } 
+    bubbleSort(arr, n);
+    int largest = arr[n - 1];
+    int secondLargest = -1;
+    
+    // Find the second largest by checking from second-last to first
+    for (int i = n - 2; i >= 0; i--) {
+        if (arr[i] < largest) {
             secondLargest = arr[i];
+            break;
         }
     }
 
-    printf("%d\n", size); 
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]); 
-    }
-    printf("\n%d\n", secondLargest); 
-}
-
-int main() {
-    int size;
-    scanf("%d", &size);
-    
-    int arr[size];
-
-    for (int i = 0; i < size; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    findSecondLargest(arr, size);
-
+    printf("%d", secondLargest);
     return 0;
 }
