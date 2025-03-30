c43bf7c8d6f7227581f79d05c396802a1e3201c0
Ishant Sharma <ishantsharma9499@gmail.com>
2025-03-30 19:07:05 +0530

    Solution to Rotate an Array to the Right by K Steps

--- Rotate an Array to the Right by K Steps@@676c02002dbc8c3e7a280b75/code.c
+++ Rotate an Array to the Right by K Steps@@676c02002dbc8c3e7a280b75/code.c
@@ -1,41 +1,32 @@
 // Your code here...
 #include <stdio.h>
 
-void rightRotate(int arr[], int n, int k) {
-    k = k % n; // Handle cases where k > n
-    int temp[k];
-
-    // Store last k elements in temp array
-    for (int i = 0; i < k; i++) {
-        temp[i] = arr[n - k + i];
-    }
-
-    // Shift remaining elements to the right
-    for (int i = n - 1; i >= k; i--) {
-        arr[i] = arr[i - k];
-    }
-
-    // Copy temp elements to the front
-    for (int i = 0; i < k; i++) {
-        arr[i] = temp[i];
-    }
-}
-
 int main() {
-    int n, k;
+    int n,r;
     scanf("%d", &n);
-
     int arr[n];
-    for (int i = 0; i < n; i++) {
+    for(int i=0; i<n; i++){
         scanf("%d", &arr[i]);
     }
-    scanf("%d", &k);
+    scanf("%d", &r);
+    if(r>=0 && r<=n){
+        for(int i = n-r; i < n; i++) {
+            printf("%d ", arr[i]);
+        }
+        for(int i = 0; i < n-r; i++){
+            printf("%d ", arr[i]);
+        }
 
-    rightRotate(arr, n, k);
-
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
     }
-
+    else{
+        r=r%n;
+        for(int i = n-r; i < n; i++) {
+            printf("%d ", arr[i]);
+        }
+        for(int i = 0; i < n-r; i++){
+            printf("%d ", arr[i]);
+        }
+    }
+    
     return 0;
 }
